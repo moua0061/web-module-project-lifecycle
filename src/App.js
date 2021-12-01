30af71db33a0c7d3a6d5fbb16db347262d50c6b9
Lia Moua <moua0061@yahoo.com>
2021-11-30 23:58:53 -0500

    added basic wireframe to site & made a handlechange event handler

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
 
 class App extends React.Component {
+
+
+
+
   render() {
-    return(<div>
-      Github Card
-    </div>);
+    return(
+    
+    <div>
+      <h1>Github Card</h1>
+      <form>
+        <input />
+        <button>search</button>
+      </form>
+
+      <div className='user-container'>
+        <img src='https://avatars.githubusercontent.com/u/82925208?v=4' />
+        <h2>Lia Moua</h2>
+        <p>
+          Total Repos: 1
+          Total Followers: 1
+        </p>
+      </div>
+      <div className='followers-container'>
+        <h2>Followers:</h2>
+        <img src='https://avatars.githubusercontent.com/u/31264591?v=4' />
+        <img src='https://avatars.githubusercontent.com/u/32182282?v=4' />
+        <img src='https://avatars.githubusercontent.com/u/43353550?v=4' />
+      </div>
+    </div>
+    
+    );
   }
 }
 
